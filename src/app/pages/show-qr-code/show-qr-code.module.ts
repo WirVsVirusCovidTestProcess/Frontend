a66cbf0ef2b2185d5fda96e837d9efbd9651f619
Bernhard Wittmann <dev@bernhardwittmann.com>
2020-03-21 22:56:56 +0100

    Revert "Double Module imports"
    This reverts commit f73cf061

--- src/app/pages/show-qr-code/show-qr-code.module.ts
+++ src/app/pages/show-qr-code/show-qr-code.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { ShowQrCodePage } from './show-qr-code';
-
+import { ShowQrCodePageRoutingModule } from './show-qr-code-routing.module';
+import { QrCodeViewComponent } from '../../components/qrcode-view/qrcode-view';
+import {QRCodeModule} from 'angularx-qrcode';
 
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
+    ShowQrCodePageRoutingModule,
+    QRCodeModule
   ],
   declarations: [
     ShowQrCodePage,
+    QrCodeViewComponent,
   ],
   entryComponents: [ShowQrCodePage],
 })
