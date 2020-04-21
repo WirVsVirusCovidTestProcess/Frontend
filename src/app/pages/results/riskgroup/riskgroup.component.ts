8b584ed6829d27e496e25968e9c77ce542224aa8
Bernhard Wittmann <dev@bernhardwittmann.com>
2020-04-21 10:40:11 +0200

    feat: Added start Over buttons to restart questionnaire
    Closes #12

--- src/app/pages/results/riskgroup/riskgroup.component.ts
+++ src/app/pages/results/riskgroup/riskgroup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { QuestionnaireDataService } from '../../../services/questionnaireData.service';
 
 @Component({
   selector: 'app-riskgroup',
@@ -8,11 +9,16 @@ import { Router } from '@angular/router';
 })
 export class RiskgroupComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private questionnaireDataService: QuestionnaireDataService) { }
 
   ngOnInit() {}
 
   submit() {
     this.router.navigate(['/signup']);
+  }
+
+  startOver(): void {
+    this.questionnaireDataService.clearQuestionnaire();
+    this.router.navigateByUrl('/', { replaceUrl: true });
   }
 }
